0d50e13e151193688ff3726f3fc3f144b131e2a7
WesleySherman <wesley.sherman@expsouthafrica.co.za>
2024-10-30 08:29:40 +0200

    Commit

---
+++ data.js
@@ -0,0 +1,784 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-01---street-view",
+      "name": "01 - Street View",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.08443034553634376,
+        "pitch": 0.020677325094439425,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.00011400378362225183,
+          "pitch": 0.21504704657268903,
+          "rotation": 0,
+          "target": "1-02---front-shop-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-02---front-shop-01",
+      "name": "02 - Front Shop 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.02148097540916183,
+        "pitch": 0.0018797568267672204,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.0860314504511166,
+          "pitch": 0.17526735928728776,
+          "rotation": 0,
+          "target": "2-03---front-shop-02"
+        },
+        {
+          "yaw": 3.088040882848161,
+          "pitch": 0.05801173873588894,
+          "rotation": 0,
+          "target": "0-01---street-view"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-03---front-shop-02",
+      "name": "03 - Front Shop 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -3.1089221022449145,
+        "pitch": 0.09123425782872374,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1314733955551954,
+          "pitch": 0.21489853552811944,
+          "rotation": 0,
+          "target": "3-04---front-shop-03"
+        },
+        {
+          "yaw": 0.09103474247672061,
+          "pitch": 0.3055140136142249,
+          "rotation": 0,
+          "target": "1-02---front-shop-01"
+        },
+        {
+          "yaw": 1.876296578909364,
+          "pitch": -0.028768472878542894,
+          "rotation": 0.7853981633974483,
+          "target": "8-09---staircase"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-04---front-shop-03",
+      "name": "04 - Front Shop 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.06175780430134026,
+        "pitch": 0.028196352401508307,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.04306252911491981,
+          "pitch": 0.2891789799020863,
+          "rotation": 0,
+          "target": "4-05---front-shop-04"
+        },
+        {
+          "yaw": -3.1107450754431696,
+          "pitch": 0.369970320904633,
+          "rotation": 0,
+          "target": "2-03---front-shop-02"
+        },
+        {
+          "yaw": -3.090742196989602,
+          "pitch": 0.18767145145969444,
+          "rotation": 0,
+          "target": "1-02---front-shop-01"
+        },
+        {
+          "yaw": -2.474086049913387,
+          "pitch": -0.10887724995309611,
+          "rotation": 7.0685834705770345,
+          "target": "8-09---staircase"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-05---front-shop-04",
+      "name": "05 - Front Shop 04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.13425609630726143,
+        "pitch": 0.06767124576361994,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.375387338979392,
+          "pitch": 0.09022832768482658,
+          "rotation": 0,
+          "target": "7-08---kitchen"
+        },
+        {
+          "yaw": 0.08692014321120212,
+          "pitch": 0.26115220552551577,
+          "rotation": 0,
+          "target": "5-06---front-shop-05"
+        },
+        {
+          "yaw": 2.975063041040931,
+          "pitch": 0.37482510303106586,
+          "rotation": 0,
+          "target": "3-04---front-shop-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-06---front-shop-05",
+      "name": "06 - Front Shop 05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -3.1029913155606135,
+        "pitch": 0.18677947378695947,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.059634215367115,
+          "pitch": 0.2679378288510694,
+          "rotation": 0,
+          "target": "4-05---front-shop-04"
+        },
+        {
+          "yaw": 2.7270533671898036,
+          "pitch": 0.130150460639193,
+          "rotation": 4.71238898038469,
+          "target": "7-08---kitchen"
+        },
+        {
+          "yaw": -0.5665138332412738,
+          "pitch": 0.08901142067131929,
+          "rotation": 0,
+          "target": "14-15---first-floor-bathroom"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-07---bathroom-ground-floor",
+      "name": "07 - Bathroom Ground Floor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.8267230276169997,
+        "pitch": 0.31625028853531845,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-08---kitchen",
+      "name": "08 - Kitchen",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -2.681431299203334,
+        "pitch": 0.09479352125782015,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.001075009047163,
+          "pitch": 0.33770446873898763,
+          "rotation": 0,
+          "target": "4-05---front-shop-04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-09---staircase",
+      "name": "09 - Staircase",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -3.0052480103089234,
+        "pitch": 0.5730113239470835,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8371352629975988,
+          "pitch": 0.6437463219072086,
+          "rotation": 0,
+          "target": "2-03---front-shop-02"
+        },
+        {
+          "yaw": 2.8246226970008728,
+          "pitch": 0.5766084553263049,
+          "rotation": 0,
+          "target": "9-10---first-floor-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-10---first-floor-01",
+      "name": "10 - First Floor 01",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.01991442026146828,
+        "pitch": 0.045493869867160086,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5839912062253276,
+          "pitch": 0.23137761678156465,
+          "rotation": 0,
+          "target": "8-09---staircase"
+        },
+        {
+          "yaw": -0.09831672368945199,
+          "pitch": 0.22194841370707508,
+          "rotation": 0,
+          "target": "10-11---first-floor-02"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-11---first-floor-02",
+      "name": "11 - First Floor 02",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -1.1262828529282949,
+        "pitch": 0.1023706998376035,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6920186775961419,
+          "pitch": 0.24400049477939412,
+          "rotation": 0,
+          "target": "11-12---first-floor-03"
+        },
+        {
+          "yaw": 2.1722454968273013,
+          "pitch": 0.29473537372024694,
+          "rotation": 0,
+          "target": "9-10---first-floor-01"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-12---first-floor-03",
+      "name": "12 - First Floor 03",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.6605399938317067,
+        "pitch": 0.10338662547219712,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9003256307326133,
+          "pitch": 0.22933093807247928,
+          "rotation": 0,
+          "target": "10-11---first-floor-02"
+        },
+        {
+          "yaw": -2.484653281823686,
+          "pitch": 0.3178998803173414,
+          "rotation": 0.7853981633974483,
+          "target": "12-13---first-floor-04"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-13---first-floor-04",
+      "name": "13 - First Floor 04",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 3.1180584334692156,
+        "pitch": 0.10172508479040765,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.111119896008251,
+          "pitch": 0.4312187387188331,
+          "rotation": 1.5707963267948966,
+          "target": "13-14---first-floor-05"
+        },
+        {
+          "yaw": 2.9644262704708426,
+          "pitch": 0.4208165300484161,
+          "rotation": 4.71238898038469,
+          "target": "11-12---first-floor-03"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-14---first-floor-05",
+      "name": "14 - First Floor 05",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.7286890602561851,
+        "pitch": 0.0354029136194427,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5001475727568536,
+          "pitch": 0.4223132419888138,
+          "rotation": 0,
+          "target": "4-05---front-shop-04"
+        },
+        {
+          "yaw": -1.0841620108111982,
+          "pitch": 0.38740923818566486,
+          "rotation": 0,
+          "target": "14-15---first-floor-bathroom"
+        },
+        {
+          "yaw": -2.292855786116192,
+          "pitch": 0.0034993045890043817,
+          "rotation": 0,
+          "target": "15-16---back-of-property"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-15---first-floor-bathroom",
+      "name": "15 - First Floor Bathroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": 0.3342976798050721,
+        "pitch": 0.20489349411762703,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9891792651216669,
+          "pitch": 0.37718575242824315,
+          "rotation": 0,
+          "target": "13-14---first-floor-05"
+        },
+        {
+          "yaw": 2.8035959092872673,
+          "pitch": 0.14963119863611674,
+          "rotation": 0,
+          "target": "15-16---back-of-property"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-16---back-of-property",
+      "name": "16 - Back of Property",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 2976,
+      "initialViewParameters": {
+        "yaw": -0.12876995093941268,
+        "pitch": 0.5530677715687435,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.033434962142259,
+          "pitch": 0.6005039879618721,
+          "rotation": 0,
+          "target": "13-14---first-floor-05"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Westbourne Road 31, Richmond Hill",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": true
+  }
+};
